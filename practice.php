0f3c42ceb1e652a12f50f691053d94315ca84246
unknowncoderboy <sammyphonist@gmail.com>
2023-03-01 13:49:08 +0100

    edited and reformatted code

--- practice.php
+++ practice.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,43 +8,43 @@
   <title>login</title>
   <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
-<?php
+  <?php
 
-if(isset($_POST['UserName'])) {
+  if (isset($_POST['UserName'])) {
 
-  $name = $_POST['UserName'];
-  $password = $_POST['Password'];
+    $name = $_POST['UserName'];
+    $password = $_POST['Password'];
 
-  $handle = fopen('names.txt', 'a');
-  $passkey = fopen('pass.txt', 'a');
+    $handle = fopen('names.txt', 'a');
+    $passkey = fopen('pass.txt', 'a');
 
-  fwrite($handle, $name."\n");
+    fwrite($handle, $name . "\n");
 
-  fwrite($passkey, $password."\n");
+    fwrite($passkey, $password . "\n");
 
-  fclose($handle); 
-  fclose($passkey);
+    fclose($handle);
+    fclose($passkey);
+  }
 
-}
+  ?>
 
-?>
-
-<div class="login">
-        <div class="inner">
-            <form action="practice.php" method="post">
-            <div class="Name">
-                <label>UserName:</label><br>
-                <input type="text" , name="UserName" id="Input" required><br>
-            </div>
-            <div class="pass">
-                <label>Password:</label> <br>
-                <input type="password" name="Password" id="mypassword" required>
-                <input type="submit" id="Mybutton" name="submit" value="Submit">
-            </div>
-        </form>
+  <div class="login">
+    <div class="inner">
+      <form action="practice.php" method="post">
+        <div class="Name">
+          <label>UserName:</label><br>
+          <input type="text" , name="UserName" id="Input" required><br>
         </div>
+        <div class="pass">
+          <label>Password:</label> <br>
+          <input type="password" name="Password" id="mypassword" required>
+          <input type="submit" id="Mybutton" name="submit" value="Submit">
+        </div>
+      </form>
     </div>
+  </div>
 </body>
-</html>
 
+</html>
